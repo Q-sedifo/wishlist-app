70011848b6eeadade726c4c090031c1645989607
Q-sedifo <oleggoper@ukr.net>
2026-01-10 15:10:56 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Tailwind працює 🚀</h1>
+    </div>
+  )
+}
+
+export default App
